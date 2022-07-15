081414ca31a084aa39996c68428221bc834fc989
Abdul Kavi <abdkavi.github@gmail.com>
2022-07-15 17:18:37 +0530

    initial commit 3

--- app/src/main/java/com/acode/jetweatherforecast/MainActivity.kt
+++ app/src/main/java/com/acode/jetweatherforecast/MainActivity.kt
@@ -20,22 +20,10 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetWeatherForecastTheme {
-        Greeting("Android")
-    }
-}
